139de4babc4656ed1bdc176d9687f820b47263bb
Neil Shevlin <neilshevlin.98@gmail.com>
2022-02-25 14:59:34 +0000

    Update app to include api test route

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,15 +2,19 @@
 
 import * as express from 'express';
 
+import { APIRoute } from './routes/api';
+
 // The App class is configured here and an instance of the object class is exported to be used by the main server.ts file.  
 class App {
     // We define two properties of the class with type annotations of the express.application object type and a number for the port.
     public app: express.Application;
     public port: number;
+    public apiRoutes: APIRoute = new APIRoute();
     
     // Assign the app property to an instance of the express.application object type.
     constructor() {
         this.app = express();
+        this.apiRoutes.routes(this.app);
         // Middleware function that has access to incoming requests and outgoing responses. Parses them as JSON.
         this.app.use(express.json());
     }
