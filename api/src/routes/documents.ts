acd4ff4dbd96a5b7fc8fe74e0206783e1e4834ec
Neil Shevlin <neilshevlin.98@gmail.com>
2022-03-03 13:10:13 +0000

    Fix documents route methods

--- api/src/routes/documents.ts
+++ api/src/routes/documents.ts
@@ -6,7 +6,7 @@ export class DocumentRoute{
     // Here the request is a GET request, but can be expanded to include other request types.
       app.route('/v1/documents').post(documentsController.postDocuments)
       app.route('/v1/documents').get(documentsController.getDocuments)
-      app.route('/v1/documents').update(documentsController.updateDocuments)
+      app.route('/v1/documents').patch(documentsController.updateDocuments)
       app.route('/v1/documents').delete(documentsController.deleteDocuments)
       app.route('/v1/document/:id').get(documentsController.getDocument)
       app.route('/v1/document/:id').post(documentsController.postDocument)
