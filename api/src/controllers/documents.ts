81d79039cf28893108ca10c770b88b3740ba123f
Neil Shevlin <neilshevlin.98@gmail.com>
2022-03-03 17:56:29 +0000

    Update documents controller to parse incoming text files

--- api/src/controllers/documents.ts
+++ api/src/controllers/documents.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
-
-
+import {APILogger} from '../utilities/logger';
+import * as bb from 'busboy'
 // Arrow function () =>{} returns a response object with a status code and a message.
 // The response object is what we used to send back some data to the client.
 export let postDocument = (req: Request, res: Response, next: NextFunction) => {
@@ -19,7 +19,30 @@ export let getDocument = (req: Request, res: Response, next: NextFunction) => {
 }
 export let postDocuments = (req: Request, res: Response, next: NextFunction) => {
     try{
-        return res.status(201).send("Added Documents")
+        
+        const busboy = bb({headers: req.headers});
+        console.log(busboy)
+        let DataFile = '';
+        // parse the multipart/form-data using busboy for typescript 
+        busboy.on('file', (name, file, info) => {
+            const {filename, encoding, mimeType} = info;
+            console.log(`File [${name}]: filename: %j, encoding: %j, mimeType: %j`,
+            filename,
+            encoding,
+            mimeType
+            );
+            DataFile = filename
+            file.on('data', (data) => {
+                console.log(`File [${name}] got ${data.length} bytes`);
+            }).on('close', () => {
+                console.log(`File [${name}] closed`);
+            })
+        });
+        busboy.on('finish', () => {
+            console.log('Done parsing form!');
+            return res.status(201).send(`Added ${DataFile}`)
+        });
+        return req.pipe(busboy); 
     }catch(error){
         return res.status(500).send(error)
     }
