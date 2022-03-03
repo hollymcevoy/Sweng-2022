f09ac6cac6685c3c2d1b319c5ac4288b3c85b9eb
Neil Shevlin <neilshevlin.98@gmail.com>
2022-03-03 13:42:09 +0000

    Update docs routes

--- api/src/routes/documents.ts
+++ api/src/routes/documents.ts
@@ -6,10 +6,9 @@ export class DocumentRoute{
     // Here the request is a GET request, but can be expanded to include other request types.
       app.route('/v1/documents').post(documentsController.postDocuments)
       app.route('/v1/documents').get(documentsController.getDocuments)
-      app.route('/v1/documents').patch(documentsController.updateDocuments)
-      app.route('/v1/documents').delete(documentsController.deleteDocuments)
-      app.route('/v1/document/:id').get(documentsController.getDocument)
-      app.route('/v1/document/:id').post(documentsController.postDocument)
+      app.route('/v1/documents/:id').patch(documentsController.updateDocuments)
+      app.route('/v1/documents/:id').delete(documentsController.deleteDocuments)
+      app.route('/v1/documents/:id').get(documentsController.getDocument)
         
     }
 }
