4cc48727cfa31542da96eecbbf5176a494d0d555
yifanzhu1592 <zhuyi@tcd.ie>
2022-03-04 14:05:41 +0000

    Corrected some function returns for questions

--- api/src/controllers/questions.ts
+++ api/src/controllers/questions.ts
@@ -5,35 +5,35 @@ import { NextFunction, Request, Response } from 'express';
 // The response object is what we used to send back some data to the client.
 export let postQuestions = (req: Request, res: Response, next: NextFunction) => {
     try{
-        return res.status(201).send()
+        return res.status(201).send("Added Document")
     }catch(error){
         return res.status(500).send(error)
     }
 }
 export let getQuestions = (req: Request, res: Response, next: NextFunction) => {
     try{
-        return res.status(201).send()
+        return res.status(200).send("Returned Documents")
     }catch(error){
         return res.status(500).send(error)
     }
 }
 export let updateQuestions = (req: Request, res: Response, next: NextFunction) => {
     try{
-        return res.status(201).send()
+        return res.status(201).send("Updated Documents")
     }catch(error){
         return res.status(500).send(error)
     }
 }
 export let deleteQuestions = (req: Request, res: Response, next: NextFunction) => {
     try{
-        return res.status(201).send()
+        return res.status(204).send("Deleted Documents")
     }catch(error){
         return res.status(500).send(error)
     }
 }
 export let getQuestion = (req: Request, res: Response, next: NextFunction) => {
     try{
-        return res.status(201).send()
+        return res.status(200).send("Returned Document")
     }catch(error){
         return res.status(500).send(error)
     }
