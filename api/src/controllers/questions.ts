109a334cc3aa35803cc0511475ec54bcdaee08fe
yifanzhu1592 <zhuyi@tcd.ie>
2022-03-05 18:33:21 +0000

    remove some get patch and delete functions for now

--- api/src/controllers/questions.ts
+++ api/src/controllers/questions.ts
@@ -10,31 +10,31 @@ export let postQuestions = (req: Request, res: Response, next: NextFunction) => 
         return res.status(500).send(error)
     }
 }
-export let getQuestions = (req: Request, res: Response, next: NextFunction) => {
-    try{
-        return res.status(200).send("Returned Documents")
-    }catch(error){
-        return res.status(500).send(error)
-    }
-}
-export let updateQuestions = (req: Request, res: Response, next: NextFunction) => {
-    try{
-        return res.status(201).send("Updated Documents")
-    }catch(error){
-        return res.status(500).send(error)
-    }
-}
-export let deleteQuestions = (req: Request, res: Response, next: NextFunction) => {
-    try{
-        return res.status(204).send("Deleted Documents")
-    }catch(error){
-        return res.status(500).send(error)
-    }
-}
-export let getQuestion = (req: Request, res: Response, next: NextFunction) => {
-    try{
-        return res.status(200).send("Returned Document")
-    }catch(error){
-        return res.status(500).send(error)
-    }
-}
+// export let getQuestions = (req: Request, res: Response, next: NextFunction) => {
+//     try{
+//         return res.status(200).send("Returned Documents")
+//     }catch(error){
+//         return res.status(500).send(error)
+//     }
+// }
+// export let updateQuestions = (req: Request, res: Response, next: NextFunction) => {
+//     try{
+//         return res.status(201).send("Updated Documents")
+//     }catch(error){
+//         return res.status(500).send(error)
+//     }
+// }
+// export let deleteQuestions = (req: Request, res: Response, next: NextFunction) => {
+//     try{
+//         return res.status(204).send("Deleted Documents")
+//     }catch(error){
+//         return res.status(500).send(error)
+//     }
+// }
+// export let getQuestion = (req: Request, res: Response, next: NextFunction) => {
+//     try{
+//         return res.status(200).send("Returned Document")
+//     }catch(error){
+//         return res.status(500).send(error)
+//     }
+// }
