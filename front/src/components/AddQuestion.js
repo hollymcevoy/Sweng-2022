16a4fc4fbecfe3a7fa6e3c378f9a470d705456d7
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2022-03-09 22:29:24 +0000

    Update AddQuestion.js

--- front/src/components/AddQuestion.js
+++ front/src/components/AddQuestion.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import "../App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import mockquestions from "../mockquestions.json"
 import { nanoid } from "nanoid";
 import axios from 'axios';
 
