496ca1e1ce4233eceb5a7c69d7cf25df9fb238d6
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2022-03-13 18:22:29 +0000

    Create EditKnowledgebase.js

---
+++ front/src/views/EditKnowledgebase.js
@@ -0,0 +1,14 @@
+import NavbarComp from '../components/NavbarComp';
+import AddQuestion from '../components/AddQuestion';
+import NavbarComp from '../components/NavbarComp';
+
+function EditKnowledgeBase() {
+  return (
+    <div>
+        <NavbarComp />
+        <AddQuestion />
+    </div>
+  );
+}
+
+export default EditKnowledgeBase;
