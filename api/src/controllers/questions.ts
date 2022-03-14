3cbd8d929af1fcbab4bbc0caa44d8ae41d37bae6
Neil Shevlin <neilshevlin.98@gmail.com>
2022-03-14 11:12:53 +0000

    Add questions posting controller logic

--- api/src/controllers/questions.ts
+++ api/src/controllers/questions.ts
@@ -1,53 +1,34 @@
 import { NextFunction, Request, Response } from 'express';
-
+import { APILogger } from '../utilities/logger';
+import fetch from 'node-fetch';
 
 // Arrow function () =>{} returns a response object with a status code and a message.
 // The response object is what we used to send back some data to the client.
-export let postQuestions = (req: Request, res: Response, next: NextFunction) => {
+export let postQuestions = async (req: Request, res: Response, next: NextFunction) => {
     try{
-        const docID = req.body.docID
-        if (!docID) {
-            return res.status(404).send("docID does not exist")
-        }
-        const questionText = req.body.questionText
-        if (!questionText) {
-            return res.status(404).send("questionText does not exist")
-        }
-        if (questionText.len() > 100) {
-            return res.status(500).send("questionText is too long")
-        }
+        const question = req.body.question
+        const docName = req.body.docName
 
-        return res.status(201).send(`A question for documentID ${docID}: ${questionText}`)
+        const api_key = process.env.AZURE_KEY;
+        const azureData = await fetch(`https://sweng-cog.cognitiveservices.azure.com/language/:query-knowledgebases?projectName=${docName}&api-version=2021-10-01`, {
+            method: 'POST',
+            headers: {
+                'OCP-APIM-Subscription-Key': api_key,
+                'Content-Type': 'application/json', 
+            },
+            body: JSON.stringify({
+                "question": question,
+                "top": 3,
+            })
+        })
+        const azureDataJson = await azureData.json();
+        if (azureData.status === 404) {
+            return res.status(404).send(`Error: ${azureDataJson.error.message}`)
+        }
+        return res.status(200).send(azureDataJson)
 
     }catch(error){
+        APILogger.logger.info(error)
         return res.status(500).send(error)
     }
 }
-// export let getQuestions = (req: Request, res: Response, next: NextFunction) => {
-//     try{
-//         return res.status(200).send("Returned Documents")
-//     }catch(error){
-//         return res.status(500).send(error)
-//     }
-// }
-// export let updateQuestions = (req: Request, res: Response, next: NextFunction) => {
-//     try{
-//         return res.status(201).send("Updated Documents")
-//     }catch(error){
-//         return res.status(500).send(error)
-//     }
-// }
-// export let deleteQuestions = (req: Request, res: Response, next: NextFunction) => {
-//     try{
-//         return res.status(204).send("Deleted Documents")
-//     }catch(error){
-//         return res.status(500).send(error)
-//     }
-// }
-// export let getQuestion = (req: Request, res: Response, next: NextFunction) => {
-//     try{
-//         return res.status(200).send("Returned Document")
-//     }catch(error){
-//         return res.status(500).send(error)
-//     }
-// }
