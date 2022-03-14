1ace1278961d062ee0f8e4e5045acfcbe976ab59
Neil Shevlin <neilshevlin.98@gmail.com>
2022-03-14 13:37:38 +0000

    retreive docs and q's from front

--- front/src/components/QnAAccordion.js
+++ front/src/components/QnAAccordion.js
@@ -3,37 +3,49 @@ import React, { Component } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Select from 'react-select';
 import { qnadummy } from '../qnadummy';
-
+import axios from 'axios';
 class QnAAccordion extends Component {
   constructor(props){
     super(props)
     this.state = {
-      selectOptions : [],
-      name: '',
-      qna: []
+      documentsList : [],
+      documentName: '',
+      documentSource: [{label: 'comp sci', source:'comp.txt'}],
+      qnas: [{question: 'what is the meaning of life?', answer: '42'}],
     }
   }
   componentDidMount(){
-    this.getOptions()
+    this.getDocuments()
   }
   handleChange(e){
-    this.setState({qna:e.qnaval, name:e.label})
+    this.setState({documentName:e.label, documentSource:e.source})
    }
-  async getOptions(){
-    const options = qnadummy.map(d => ({
-      "label" : d.name,
-      "qnaval" : d.qna
-    }))
-    this.setState({selectOptions: options})
+  async getDocuments (){
+    const response = await axios.get('http://localhost:3000/v1/documents');
+    const documents = response.data.value;
+
+    const documentsList = documents.reduce((res, doc) => {
+      if(doc.displayName){
+        res.push({label: doc.displayName, source:doc.source, sourceUri: doc.sourceUri});
+      }
+      return res;
+    },[])
+
+    this.setState({documentsList: documentsList})
+  }
+  async getQuestions (){
+    const response = await axios.get('http://localhost:3000/v1/questions');
+    const questions = response.questions;
+    this.setState({questions: questions})
   }
   render() {
     return (
       <div>
-        <Select options={this.state.selectOptions} onChange={this.handleChange.bind(this)} />
+        <Select options={this.state.documentsList} onChange={this.handleChange.bind(this)} />
 
         <Accordion className="Accordion" defaultActiveKey="0">
             <div>
-              {this.state.qna.map((data, key) => {
+              {this.state.qnas.map((data, key) => {
                 return (
                   <div key={key}>
                     <Accordion.Item eventKey={key+1}>
