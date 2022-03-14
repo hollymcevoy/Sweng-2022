d846002df3e9ae144139bc5f2e45c63cca284cf8
Neil Shevlin <neilshevlin.98@gmail.com>
2022-03-14 13:39:25 +0000

    add get questions route

--- api/src/routes/questions.ts
+++ api/src/routes/questions.ts
@@ -6,5 +6,6 @@ export class QuestionsRoute{
     public routes(app):void{
     // Here the request is a GET request, but can be expanded to include other request types.
       app.route('/v1/questions').post(questionsController.postQuestions)
+      app.route('/v1/questions').get(questionsController.getQuestions)
     }
   }
