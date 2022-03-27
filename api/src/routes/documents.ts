a5c72e4888dae471e7fd996d2903ae5099a20ade
Neil Shevlin <neilshevlin.98@gmail.com>
2022-03-27 13:45:13 +0100

    Add auth0 middleware

--- api/src/routes/documents.ts
+++ api/src/routes/documents.ts
@@ -1,14 +1,28 @@
 import * as documentsController from '../controllers/documents';
-
+import { authMiddleware } from '../utilities/auth0';
 // Export a class that has a property for request handlers. 
 export class DocumentRoute{
     public routes(app):void{
     // Here the request is a GET request, but can be expanded to include other request types.
-      app.route('/v1/documents').post(documentsController.postDocuments)
-      app.route('/v1/documents').get(documentsController.getDocuments)
-      app.route('/v1/documents/:id').patch(documentsController.updateDocuments)
-      app.route('/v1/documents/:id').delete(documentsController.deleteDocuments)
-      app.route('/v1/documents/:id').get(documentsController.getDocument)
-        
+      app.route('/v1/documents').post(
+        authMiddleware,
+        documentsController.postDocuments
+      )
+      app.route('/v1/documents').get(
+        authMiddleware,
+        documentsController.getDocuments
+      )
+      app.route('/v1/documents/:id').patch(
+        authMiddleware,
+        documentsController.updateDocuments
+      )
+      app.route('/v1/documents/:id').delete(
+        authMiddleware,
+        documentsController.deleteDocuments
+      )
+      app.route('/v1/documents/:id').get(
+        authMiddleware,
+        documentsController.getDocument
+      ) 
     }
 }
