8337dd6610d3b474ca5a752bce17f68d8ef2a5ce
Neil Shevlin <neilshevlin.98@gmail.com>
2022-04-10 11:11:07 +0100

    Add new build files

--- api/src/controllers/documents.ts
+++ api/src/controllers/documents.ts
@@ -4,13 +4,6 @@ import * as bb from 'busboy'
 import fetch from 'node-fetch';
 // Arrow function () =>{} returns a response object with a status code and a message.
 // The response object is what we used to send back some data to the client.
-export let postDocument = (req: Request, res: Response, next: NextFunction) => {
-    try{
-        return res.status(201).send("Added Document")
-    }catch(error){
-        return res.status(500).send(error)
-    }
-}
 
 export let getDocumentStatus = async (req: Request, res: Response, next: NextFunction) => {
     try{ 
